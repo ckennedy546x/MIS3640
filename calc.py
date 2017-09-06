20567ccbcb37406e7910c43be9e4a79d78e52090
Chris Kennedy <31513373+ckennedy546x@users.noreply.github.com>
2017-09-06 15:44:00 -0400

    Finished Exercise from Session 2

--- calc.py
+++ calc.py
@@ -1,5 +1,13 @@
-print(60*42+42)
+print(60*42+42) #Beginning of Exercise for Session 1
 print(10/1.61)
 print(6.21/2562)
 print(.00242388*60)
 print(.1454327*60)
+
+print((4/3)*3.1415926*5*5*5) #Beginning of Exercise for Session 2 #1 
+print((24.95*.6*60)+(59*.75)+3)#2
+print(((8*60+15)*2)+((7*60+12)*3))#3
+print(2286/60)
+print('7:30:06AM')#Home arrival time
+print((89-82)/82*100)
+print('Percentage of grade increase is: %02.0d %%' % 8.5365)
---
+++ hello.py
@@ -0,0 +1,9 @@
+
+
+message = 'I learned something cool!'
+n = 100
+print(n + 100)
+
+'Hello, %s' % 'world'
+print('Today is %2d-%02d.' % (9, 6))
+print('Pi equals %.2f.' % 3.1415926)
